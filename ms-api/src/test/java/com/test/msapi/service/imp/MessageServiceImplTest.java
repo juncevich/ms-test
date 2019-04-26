00d66b0258d741b3c831102a2ec4aa69898192cb
Yuntsevich Aleksandr Sergeevich <a.yuntsevich@tinkoff.ru>
2019-04-26 18:12:04 +0500

    Dockerise

---
+++ ms-api/src/test/java/com/test/msapi/service/imp/MessageServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.test.msapi.service.imp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MessageServiceImplTest {
+    private static final String RESULTS = "[" +
+            "{\"localDateTime\":\"2019-04-26T16:24:06.599\",\"value\":\"74154cf1-20c2-4826-aa8f-598f30d7aea0\"}," +
+            "{\"localDateTime\":\"2019-04-26T16:24:07.601\",\"value\":\"aea4c6d2-8bc0-4145-8b28-e84d5df8879a\"}," +
+            "{\"localDateTime\":\"2019-04-26T16:24:08.603\",\"value\":\"996cf9d6-daf6-4c89-86ee-18ccef16a5d5\"}," +
+            "{\"localDateTime\":\"2019-04-26T16:24:09.605\",\"value\":\"1f856185-6fa2-4426-a20c-694c075b57a4\"}]";
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @InjectMocks
+    private MessageServiceImpl messageService;
+
+    @Test
+    public void testSendGetResultRequest() {
+        given(rabbitTemplate.convertSendAndReceive(
+                any(String.class), any(String.class), any(String.class), any(CorrelationData.class))
+        ).willReturn(RESULTS);
+
+        String results = messageService.sendGetResultRequest();
+        assertEquals(RESULTS, results);
+    }
+}
